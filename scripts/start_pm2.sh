906c1dbba5985e56f3534e254219320bc5c1cd7b
Vipin kumar <7835sha@gmail.com>
2025-06-25 13:58:43 +0000

    Update start_pm2.sh

--- scripts/start_pm2.sh
+++ scripts/start_pm2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /var/www/html/Movix
-npm install
-pm2 delete all || true
-pm2 start npm --name movix-app -- run dev
+
+# Change to the project directory (optional if the script is run from there)
+cd /var/www/html/Movix || exit
+
+# Run the npm build command with sudo
+sudo npm run build
